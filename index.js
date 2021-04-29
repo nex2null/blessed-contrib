5de43e5f66fd1584125bd229717789e0bf200804
nex2null <nex2null@gmail.com>
2021-04-29 07:22:27 -0400

    Adding a modified question prompt (#2)
    * Adding question widget
    
    * Finishing up work on the question prompt
    
    * Removing thing

--- index.js
+++ index.js
@@ -25,3 +25,5 @@ exports.OutputBuffer = require('./lib/server-utils').OutputBuffer
 exports.InputBuffer = require('./lib/server-utils').InputBuffer
 exports.createScreen = require('./lib/server-utils').createScreen
 exports.serverError = require('./lib/server-utils').serverError
+
+exports.question = require('./lib/widget/question');
